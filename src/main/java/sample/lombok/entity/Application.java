c936787ca83c3d8846347d830b98f34445a0ec8a
duanyinglong <kylerduane@163.com>
2019-10-12 17:20:46 +0800

    init

---
+++ src/main/java/sample/lombok/entity/Application.java
@@ -0,0 +1,13 @@
+package sample.lombok.entity;
+
+/**
+ * @author maxD
+ */
+public class Application {
+    public static void main(String[] args) {
+        SampleData sampleData = new SampleData();
+        sampleData.setId("1");
+        sampleData.setCode("code1");
+        sampleData.setName("name1");
+    }
+}
---
+++ src/main/java/sample/lombok/entity/SampleData.java
@@ -0,0 +1,15 @@
+package sample.lombok.entity;
+
+import lombok.Data;
+
+/**
+ * @author maxD
+ */
+@Data
+public class SampleData {
+    private String id;
+
+    private String name;
+
+    private String code;
+}
